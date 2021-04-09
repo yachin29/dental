09303a11d272d26f34d43a8f4437032f19f3d5c6
矢島智明 <yachin29@gmail.com>
2021-04-09 19:48:16 +0900

    とりあえず

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//ハンバーガー
+
+$('#ham-btn').on('touchstart ',function(){
+$(this).toggleClass('click');
+
+$('#g-nav-sp').fadeToggle();
+
+});
+
+var windowWidth = $(window).width();
+
+$(window).on('load resize', function(){
+  var ww = $(window).width();
+  if(windowWidth != ww) {
+    if($(window).outerWidth() < 768 ){
+    //スマホの時の状態
+    $('#g-nav-sp').hide();
+  }else{
+    //タブレット・PCの時の状態
+    $('#ham-btn').removeClass('click');
+  };
+   windowWidth = ww;
+  };
+});
